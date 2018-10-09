3d8968570bfcbb02149739ee98495d008a886c5e
Hinrich Kaestner <hinrich.kaestner@gmail.com>
2018-10-09 16:35:52 +0200

    WIP spider for Ricardo

--- moto/moto/settings.py
+++ moto/moto/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'moto.spiders'
 #USER_AGENT = 'moto (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -88,3 +88,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_EXPORT_ENCODING = 'utf-8'
---
+++ moto/moto/spiders/ricardo_spider.py
@@ -0,0 +1,37 @@
+import scrapy
+
+
+class RicardoSpider(scrapy.Spider):
+    name = "ricardo"
+
+    def start_requests(self):
+        urls = [
+            'https://auto.ricardo.ch/de/s/moto',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_overview)
+
+    def parse_overview(self, response):
+        article_pages = response.css("a.ric-article::attr(href)")
+        overview_pages = response.css("div.ric-pagination a::attr(href)")
+
+        # go to article page
+        for article_page in article_pages:
+            if article_page is not None:
+                yield response.follow(article_page, callback=self.parse_article)
+
+        # go to next overview page
+        for overview_page in overview_pages:
+            if overview_page is not None:
+                yield response.follow(overview_page, callback=self.parse_overview)
+
+    def parse_article(self, response):
+        yield {
+            'title': response.css("div.title h1::text").extract_first(),
+            'subtitle': response.css("div.title h4.subtitle::text").extract_first(),
+            'performance': int(response.css("div.power div.value::text").extract_first().replace(" PS", "")),
+            'mileage': float(response.css("div.mileage div.value::text").extract_first().replace("'", "").replace(" km", "")),
+            'registration': response.css("div.registration div.value::text").extract_first(),
+            'price': float(response.css("div.price span:last-of-type::text").extract_first().replace("'", "")),
+            'description': response.css("#article-description::text").extract_first(),
+        }
